ae63a628dc021998cb2fe7d19c3a842bb781aee7
8099751086 <dilwarhussainit24@gmail.com>
2022-08-01 17:33:50 +0530

    Present Or Absent

---
+++ src/com/Employee/wages/UC1_PresentOrAbsent.java
@@ -0,0 +1,22 @@
+package com.Employee.wages;
+
+public class UC1_PresentOrAbsent {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Attendance");
+		 int Attendance=1; 
+		  double randomCheck= (Math.floor(Math.random()*10))%2;
+		  if (Attendance==randomCheck) {
+			  System.out.println("Employee is present");
+			  
+		  } else {
+			  System.out.println("Employee is absent");
+		  }
+
+			}
+
+			}
+	
+
+
