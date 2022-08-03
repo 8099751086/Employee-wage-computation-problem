d1fa1ed8af477c0b0ee6ab964170720460b8d8ec
8099751086 <dilwarhussainit24@gmail.com>
2022-08-04 01:24:27 +0530

    Wage Per Day

--- src/com/Employee/wages/UC2_DailyEmpoloyee_Wages.java
+++ src/com/Employee/wages/UC2_DailyEmpoloyee_Wages.java
@@ -1,25 +1,20 @@
 package com.Employee.wages;
 
-public class Employee_Wages {
-	public static void main(String args[]) {
+public class UC2_DailyEmpoloyee_Wages {
 
-		System.out.println("Welcome to Employee Computation");
-
-		Employee_Wages Wage = new Employee_Wages();
-		Wage.Employee();
-	}
-
-	public void Employee() {
+	public static void main(String[] args) {
 		int noOfhourperday = 8;
 		int Wageperhr = 20;
+
 		int Attendance = 1;
 		double randomCheck = (Math.floor(Math.random() * 10)) % 2;
+
 		if (Attendance == randomCheck) {
 
 			if (noOfhourperday == 8) {
 				System.out.println("Employee is present");
 				int totalWage = noOfhourperday * Wageperhr;
-				System.out.println("wage per day is:" + totalWage);
+				System.out.println("wage per day is:" +  totalWage);
 
 			}
 		}
@@ -27,6 +22,7 @@ public class Employee_Wages {
 		else {
 			System.out.println(" Employee is Absent");
 		}
+
 	}
 
 }
